da0936e39c7af888e661c31f8e807da159a6cfd7
Liam Vovk <liam.vovk@gmail.com>
2019-02-14 22:51:41 -0500

    Updates

---
+++ precache-manifest.415f7e28857ae941f5014b3d6845f067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26bd1dbd286e70ee7991",
+    "url": "/static/js/main.26bd1dbd.chunk.js"
+  },
+  {
+    "revision": "4f3336d024270b662380",
+    "url": "/static/js/1.4f3336d0.chunk.js"
+  },
+  {
+    "revision": "26bd1dbd286e70ee7991",
+    "url": "/static/css/main.26b0d7ba.chunk.css"
+  },
+  {
+    "revision": "7fd44afc68275d91ff0441cc300bf09e",
+    "url": "/index.html"
+  }
+];
