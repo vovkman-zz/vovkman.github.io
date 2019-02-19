54ce278f0aa57fafc88569d3f54aa50332675d2d
Liam Vovk <liam.vovk@gmail.com>
2019-02-18 22:58:36 -0500

    Updates

---
+++ precache-manifest.9520b351c7628c6bf9b4b58579fe755a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76d80c565a05f252d51a",
+    "url": "/static/js/main.76d80c56.chunk.js"
+  },
+  {
+    "revision": "bb77cd31a3fa752d1ec6",
+    "url": "/static/js/1.bb77cd31.chunk.js"
+  },
+  {
+    "revision": "76d80c565a05f252d51a",
+    "url": "/static/css/main.b454fb45.chunk.css"
+  },
+  {
+    "revision": "820d7e8c156d35f7b69e738f7da6031f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e28ed9beacf67b0caf9cfe8462d1bec.js"
+  "/precache-manifest.9520b351c7628c6bf9b4b58579fe755a.js"
 );
 
 workbox.clientsClaim();
